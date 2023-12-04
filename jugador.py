eef3b5da046d8d9197ce71ef53f62f0def08ec4d
pochetes11 <matiasarielmosquera@gmail.com>
2023-12-04 10:05:55 -0300

    jugador.py

--- jugador.py
+++ jugador.py
@@ -1,6 +1,6 @@
 class Jugador:
-    def __init__(self, nombre, fichas=5):
-        self.nombre = nombre
+    def __init__(self, nombre, fichas=5):  #funcion q crea 
+        self.nombre = nombre                #self.fichas = crear.fichas
         self.fichas = fichas
 
     def darFicha(self, cuantas=1):
@@ -17,5 +17,5 @@ class Jugador:
     def sinFichas(self):
         return self.fichas == 0
 
-    def __str__(self):
+    def __str__(self):                #funcion que permite mostrar el mensaje 
         return f"{self.nombre}, {self.fichas} fichas"
