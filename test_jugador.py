444c57fbb4d9ef4c4296e80250bc3657c5777ef9
pochetes11 <matiasarielmosquera@gmail.com>
2023-12-04 10:06:46 -0300

    test_jugador.py

--- test_jugador.py
+++ test_jugador.py
@@ -1,31 +1,26 @@
-import unittest
 from jugador import Jugador
+import pytest
 
-class TestJugador(unittest.TestCase):
-    def test_darFicha_agrega_correctamente(self):
-        jugador = Jugador("Sol", 8)
-        jugador.darFicha(3)
-        self.assertEqual(jugador.fichas, 11)
+def test_dar_ficha_agrega_correctamente():
+    jugador = Jugador("Sol", 8)
+    jugador.darFicha(3)
+    assert jugador.fichas == 11
 
-    def test_sacarFicha_quita_correctamente(self):
-        jugador = Jugador("Luna", 6)
-        jugador.sacarFicha(2)
-        self.assertEqual(jugador.fichas, 4)
+def test_sacar_ficha_quita_correctamente():
+    jugador = Jugador("Luna", 6)
+    jugador.sacarFicha(2)
+    assert jugador.fichas == 4
 
-    def test_tieneFicha_funciona_correctamente(self):
-        jugador = Jugador("Roma", 10)
-        self.assertTrue(jugador.tieneFicha(5))
-        self.assertFalse(jugador.tieneFicha(12))
+def test_tiene_ficha_funciona_correctamente():
+    jugador = Jugador("Roma", 10)
+    assert jugador.tieneFicha(5)
+    assert not jugador.tieneFicha(12)
 
-    def test_sinFichas_devuelve_correctamente(self):
-        jugador = Jugador("Ares", 3)
-        self.assertFalse(jugador.sinFichas())
-        jugador.sacarFicha(3)
-        self.assertTrue(jugador.sinFichas())
+def test_sin_fichas_devuelve_correctamente():
+    jugador = Jugador("Ares", 3)
+    assert not jugador.sinFichas()
+    jugador.sacarFicha(3)
+    assert jugador.sinFichas()
 
-    def test_str_muestra_correctamente(self):
-        jugador = Jugador("Apolo", 7)
-        self.assertEqual(str(jugador), "Apolo, 7 fichas")
-
-if __name__ == '__main__':
-    unittest.main()
+def test_str_muestra_correctamente():
+    jugador = Jugador("Apolo", 7)
