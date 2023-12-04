5d6eea740181442b65cb558f02d9567689192a5f
pochetes11 <matiasarielmosquera@gmail.com>
2023-12-04 10:06:54 -0300

    test_apuesta.py

--- test_apuesta.py
+++ test_apuesta.py
@@ -1,41 +1,37 @@
-import unittest
 from apuesta import Apuesta
+import pytest
 
-class TestApuesta(unittest.TestCase):
-    def test_ponerFicha_agrega_correctamente(self):
-        apuesta = Apuesta()
-        apuesta.ponerFicha(3)
-        self.assertEqual(apuesta.fichas, 3)
+def test_ponerFicha_agrega_correctamente():
+    apuesta = Apuesta()
+    apuesta.ponerFicha(3)
+    assert apuesta.fichas == 3
 
-    def test_tomarFicha_quita_correctamente(self):
-        apuesta = Apuesta()
-        apuesta.ponerFicha(5)
-        apuesta.tomarFicha(3)
-        self.assertEqual(apuesta.fichas, 2)
+def test_tomarFicha_quita_correctamente():
+    apuesta = Apuesta()
+    apuesta.ponerFicha(5)
+    apuesta.tomarFicha(3)
+    assert apuesta.fichas == 2
 
-    def test_tomarTodas_devuelve_correctamente(self):
-        apuesta = Apuesta()
-        apuesta.ponerFicha(7)
-        todas = apuesta.tomarTodas()
-        self.assertEqual(todas, 7)
-        self.assertEqual(apuesta.fichas, 0)
+def test_tomarTodas_devuelve_correctamente():
+    apuesta = Apuesta()
+    apuesta.ponerFicha(7)
+    todas = apuesta.tomarTodas()
+    assert todas == 7
+    assert apuesta.fichas == 0
 
-    def test_tieneFicha_funciona_correctamente(self):
-        apuesta = Apuesta()
-        apuesta.ponerFicha(4)
-        self.assertTrue(apuesta.tieneFicha(2))
-        self.assertFalse(apuesta.tieneFicha(6))
+def test_tieneFicha_funciona_correctamente():
+    apuesta = Apuesta()
+    apuesta.ponerFicha(4)
+    assert apuesta.tieneFicha(2)
+    assert not apuesta.tieneFicha(6)
 
-    def test_estaVacia_devuelve_correctamente(self):
-        apuesta = Apuesta()
-        self.assertTrue(apuesta.estaVacia())
-        apuesta.ponerFicha(2)
-        self.assertFalse(apuesta.estaVacia())
+def test_estaVacia_devuelve_correctamente():
+    apuesta = Apuesta()
+    assert apuesta.estaVacia()
+    apuesta.ponerFicha(2)
+    assert not apuesta.estaVacia()
 
-    def test_str_muestra_correctamente(self):
-        apuesta = Apuesta()
-        apuesta.ponerFicha(5)
-        self.assertEqual(str(apuesta), "Apuesta: 5 fichas")
-
-if __name__ == '__main__':
-    unittest.main()
+def test_str_muestra_correctamente():
+    apuesta = Apuesta()
+    apuesta.ponerFicha(5)
+    assert str(apuesta) == "Apuesta: 5 fichas"
